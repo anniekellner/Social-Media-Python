6cef2687e7475663121ba4072f8625dcdd7a5c11
anniekellner <annie.kellner@colostate.edu>
2020-01-24 10:45:15 -0700

    test - will this show up in GitHub?

---
+++ Create_SQL_tweetsDB.py
@@ -0,0 +1,36 @@
+######################################################
+##One-time database creation
+import sqlite3
+
+conn = sqlite3.connect('C:\\Users\\akell\\OneDrive - Colostate\\PhD\\Ch4_Twitter\\Data\\Bears_CO.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE tweets
+    (tweetID integer primary key,
+    tweetDate datetime,
+    user text,
+    user_id text,
+    user_desc text,
+    user_loc text,
+    user_verified text,
+    user_url text,
+    user_followers integer,
+    user_listed integer,
+    user_statuses integer,
+    user_friends integer,
+    user_lang text,
+    user_fav integer,
+    user_geo text,
+    tweet_lat real,
+    tweet_long real,
+    tweetText text,
+    links text,
+    linked_photo text,
+    media_url text,
+    hashtags text,
+    tweet_location text
+    )''')
+conn.commit()
+conn.close()
+
+##End one-time database creation
+######################################################
